e893c655e00019c3c4fabae2eff40d985e3d2a5e
JS-3 <106467278+JS-3@users.noreply.github.com>
2022-06-11 02:01:36 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let raceNumber = Math.floor(Math.random() * 1000);
+
+var raceTime = false;
+
+const runnersAge = 15;
+
+if(runnersAge >= 18 && raceTime === true) {
+    raceNumber = raceNumber + 1000};
+
+if(runnersAge >= 18 && raceTime === true) {
+    console.log(`Your race will begin at 9:30 am runner ${raceNumber}. `);
+}else if(runnersAge >= 18 && raceTime === false) {
+    console.log(`Your race will begin at 11:00 am runner ${raceNumber}.`)};
+
+if(runnersAge < 18) console.log(`Your race will begin at 12:30 pm ${raceNumber}.`);
